26e080f306e5a56703ca4232fc7751978f9d5158
coding-to-music <connors.tom@gmail.com>
2022-12-12 21:33:21 +0000

    Build

--- app.ts
+++ app.ts
@@ -1,13 +1,15 @@
-import express from "express";
+import "dotenv/config";
+
 import { NextFunction, Request, Response } from "express";
-import bodyParser from "body-parser";
-import postsRouter from "./routes/posts-routes";
+
 import HttpError from "./models/http-error";
-import userRouter from "./routes/users-routes";
+import bodyParser from "body-parser";
+import commentsRouter from "./routes/comments-routes";
+import express from "express";
 import fallBackRouter from "./routes/fallback-routes";
 import mongoose from "mongoose";
-import "dotenv/config";
-import commentsRouter from "./routes/comments-routes";
+import postsRouter from "./routes/posts-routes";
+import userRouter from "./routes/users-routes";
 
 const app = express();
 // parses any incoming data, converts from JSON to regular JS object notation and calls next
@@ -16,7 +18,10 @@ app.use(bodyParser.json());
 app.use((req: Request, res: Response, next: NextFunction) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
 
   next();
 });
@@ -32,34 +37,42 @@ app.use((req: Request, res: Response, next: NextFunction) => {
   throw error;
 });
 
-app.use((error: NodeJS.ErrnoException, req: Request, res: Response, next: NextFunction) => {
-  if (req.file) {
-    console.log(req.file);
-    //TODO figure out how to delete from S3 bucket
-    // fs.unlink(req.file.path, (err) => {
-    //   console.warn("file deleted");
-    // });
-  }
-  if (res.headersSent) {
-    return next(error);
-  }
+app.use(
+  (
+    error: NodeJS.ErrnoException,
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    if (req.file) {
+      console.log(req.file);
+      //TODO figure out how to delete from S3 bucket
+      // fs.unlink(req.file.path, (err) => {
+      //   console.warn("file deleted");
+      // });
+    }
+    if (res.headersSent) {
+      return next(error);
+    }
 
-  if (error.code == "LIMIT_FILE_SIZE") {
-    res.status(422);
-    res.json({ message: "File size too large (Please stay below 10mb)" });
+    if (error.code == "LIMIT_FILE_SIZE") {
+      res.status(422);
+      res.json({ message: "File size too large (Please stay below 10mb)" });
+    }
+    res.status((error.code as unknown as number) || 500);
+    res.json({ message: error.message || "An unknown error occured" });
   }
-  res.status((error.code as unknown as number) || 500);
-  res.json({ message: error.message || "An unknown error occured" });
-});
+);
 
+// `mongodb+srv://${process.env.DB_CRED}:${process.env.DB_CRED}@cluster0.pku1z.mongodb.net/instasham?retryWrites=true&w=majority`
+
+const DB = process.env.MONGO_URI;
 mongoose
-  .connect(
-    `mongodb+srv://${process.env.DB_CRED}:${process.env.DB_CRED}@cluster0.pku1z.mongodb.net/instasham?retryWrites=true&w=majority`
-  )
+  .connect(DB)
   .then(() => {
     const port = process.env.PORT || 5000;
     app.listen(port, () => {
-      console.log(`API running on PORT ${port}`);
+      console.log(`API running on http://localhost:${port}`);
     });
   })
   .catch((error) => {
